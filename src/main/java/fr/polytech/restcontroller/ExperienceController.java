dce05c846df6b1eb23f3d181b2b20077a1251e85
romainfrezier <romainfrezier@gmail.com>
2023-11-03 14:29:50 +0100

    feat(Rest API): Adding rest routes for the experience-api.

--- src/main/java/fr/polytech/restcontroller/ExperienceController.java
+++ src/main/java/fr/polytech/restcontroller/ExperienceController.java
@@ -1,11 +1,19 @@
 package fr.polytech.restcontroller;
 
+import fr.polytech.model.Experience;
+import fr.polytech.model.ExperienceDTO;
 import fr.polytech.service.ExperienceService;
+import jakarta.ws.rs.NotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/experience")
@@ -18,4 +26,98 @@ public class ExperienceController {
 
     @Autowired
     private ExperienceService experienceService;
+
+    /**
+     * Get all experiences.
+     *
+     * @return List of all experiences.
+     */
+    @GetMapping("/")
+    @PreAuthorize("hasRole('client_admin')")
+    public ResponseEntity<List<Experience>> getAllExperiences() {
+        try {
+            List<Experience> experiences = experienceService.getAllExperiences();
+            logger.info("Got all experiences");
+            return ResponseEntity.ok(experiences);
+        } catch (Exception e) {
+            logger.error("Error while getting all experiences: " + e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Get experience by id.
+     *
+     * @param id Experience id.
+     * @return Experience with the specified id.
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<Experience> getExperienceById(@PathVariable("id") UUID id) {
+        try {
+            Experience experience = experienceService.getExperienceById(id);
+            logger.info("Got experience with id " + id);
+            return ResponseEntity.ok(experience);
+        } catch (NotFoundException e) {
+            logger.error("Error while getting experience with id " + id + ": " + e.getMessage());
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    /**
+     * Create an experience.
+     *
+     * @param experience Experience to create.
+     * @return Created experience.
+     */
+    @PostMapping("/")
+    @PreAuthorize("hasRole('client_candidate')")
+    public ResponseEntity<Experience> createExperience(@RequestBody ExperienceDTO experience) {
+        try {
+            Experience createdExperience = experienceService.createExperience(experience);
+            logger.info("Created experience with id " + createdExperience.getId());
+            return ResponseEntity.ok(createdExperience);
+        } catch (Exception e) {
+            logger.error("Error while creating experience: " + e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Update an experience.
+     *
+     * @param id         Experience id.
+     * @param experience Experience to update.
+     * @return Updated experience.
+     */
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('client_candidate')")
+    public ResponseEntity<Experience> updateExperience(@PathVariable("id") UUID id, @RequestBody ExperienceDTO experience) {
+        try {
+            Experience updatedExperience = experienceService.updateExperience(id, experience);
+            logger.info("Updated experience with id " + id);
+            return ResponseEntity.ok(updatedExperience);
+        } catch (NotFoundException e) {
+            logger.error("Error while updating experience with id " + id + ": " + e.getMessage());
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    /**
+     * Delete an experience.
+     *
+     * @param id Experience id.
+     * @return True if the experience has been deleted, false otherwise.
+     */
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('client_candidate')")
+    public ResponseEntity<Boolean> deleteExperience(@PathVariable("id") UUID id) {
+        try {
+            experienceService.deleteExperience(id);
+            logger.info("Deleted experience with id " + id);
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        } catch (NotFoundException e) {
+            logger.error("Error while deleting experience with id " + id + ": " + e.getMessage());
+            return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- src/main/java/fr/polytech/service/ExperienceService.java
+++ src/main/java/fr/polytech/service/ExperienceService.java
@@ -1,10 +1,16 @@
 package fr.polytech.service;
 
+import fr.polytech.model.Experience;
+import fr.polytech.model.ExperienceDTO;
 import fr.polytech.repository.ExperienceRepository;
+import jakarta.ws.rs.NotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ExperienceService {
@@ -16,4 +22,103 @@ public class ExperienceService {
 
     @Autowired
     private ExperienceRepository experienceRepository;
+
+    /**
+     * Get all experiences.
+     *
+     * @return List of all experiences.
+     */
+    public List<Experience> getAllExperiences() {
+        logger.info("Getting all experiences");
+        return experienceRepository.findAll();
+    }
+
+    /**
+     * Get experience by id.
+     *
+     * @param id Experience id.
+     * @return Experience with the specified id.
+     * @throws NotFoundException If the experience is not found.
+     */
+    public Experience getExperienceById(UUID id) throws NotFoundException {
+        logger.info("Getting experience with id " + id);
+        Experience experience = experienceRepository.findById(id).orElse(null);
+
+        if (experience == null) {
+            logger.error("Error while getting an experience: experience not found");
+            // If the experience is not found, throw an exception
+            throw new NotFoundException("Experience not found");
+        }
+
+        logger.debug("Returning experience with id " + id);
+        return experience;
+    }
+
+    /**
+     * Create an experience.
+     *
+     * @param experience Experience to create.
+     * @return Created experience.
+     */
+    public Experience createExperience(ExperienceDTO experience) {
+        logger.info("Creating experience");
+
+        Experience newExperience = new Experience();
+        newExperience.setCompanyId(experience.getCompanyId());
+        newExperience.setJobTitle(experience.getJobTitle());
+        newExperience.setJobCategoryId(experience.getJobCategoryId());
+        newExperience.setStartDate(experience.getStartDate());
+        newExperience.setEndDate(experience.getEndDate());
+
+        return experienceRepository.save(newExperience);
+    }
+
+    /**
+     * Update an experience.
+     *
+     * @param id         Experience id.
+     * @param experience Experience to update.
+     * @return Updated experience.
+     * @throws NotFoundException If the experience is not found.
+     */
+    public Experience updateExperience(UUID id, ExperienceDTO experience) throws NotFoundException {
+        logger.info("Updating experience with id " + id);
+
+        Experience updatedExperience = getExperienceById(id);
+
+        if (updatedExperience == null) {
+            logger.error("Error while updating an experience: experience not found");
+            // If the experience is not found, throw an exception
+            throw new NotFoundException("Experience not found");
+        }
+
+        updatedExperience.setCompanyId(experience.getCompanyId());
+        updatedExperience.setJobTitle(experience.getJobTitle());
+        updatedExperience.setJobCategoryId(experience.getJobCategoryId());
+        updatedExperience.setStartDate(experience.getStartDate());
+        updatedExperience.setEndDate(experience.getEndDate());
+
+        return experienceRepository.save(updatedExperience);
+    }
+
+    /**
+     * Delete an experience.
+     *
+     * @param id Experience id.
+     * @throws NotFoundException If the experience is not found.
+     */
+    public void deleteExperience(UUID id) throws NotFoundException {
+        logger.info("Deleting experience with id " + id);
+
+        Experience experience = getExperienceById(id);
+
+        if (experience == null) {
+            logger.error("Error while deleting an experience: experience not found");
+            // If the experience is not found, throw an exception
+            throw new NotFoundException("Experience not found");
+        }
+
+        logger.debug("Deleting experience with id " + id);
+        experienceRepository.delete(experience);
+    }
 }
